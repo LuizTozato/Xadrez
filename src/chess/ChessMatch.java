376691fafdeecdc9bc9cb217480967479ce5e624
Luiz Tozato <lfntozato@gmail.com>
2020-07-13 20:25:16 -0300

    Pos bug no terminal

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,15 +24,26 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	/*função que recebe: 1 peça em coordenadas em batalha naval.
+	
+	/*função que recebe: 1 peça em coordenadas de batalha naval.
 	faz: transforma em coordenadas de matriz e instancia a peça no board.*/
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b',6, new Rook(board, Color.WHITE));
-		placeNewPiece('e',8,new King(board, Color.BLACK));
-		placeNewPiece('e',1,new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }

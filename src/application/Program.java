2acc1f728e340d319dca47b8e59aa075a12e2be4
Luiz Tozato <lfntozato@gmail.com>
2020-07-14 14:20:12 -0300

    Tratando exceções e limpando a tela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -9,30 +11,38 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		
-		while(true){
-			
-			UI.printBoard(chessMatch.getPieces());
-			
-			System.out.println();
-			
-			//O usuário diz qual é a origem da peça
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			
-			//O usuário diz qual é o destino da peça
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-		
-		
+
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+
+				System.out.println();
+
+				// O usuário diz qual é a origem da peça
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+
+				// O usuário diz qual é o destino da peça
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} 
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); //aguardar o usuário digitar Enter
+			} 
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine(); //aguardar o usuário digitar Enter
+			}
+
 		}
 	}
 
